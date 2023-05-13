8a7190c88db37bd3fb5aac3d51311c1affbf5f94
unknown <example@mail.com>
2023-05-13 04:07:05 +0100

    more refactoring

--- src/stream/campaign-creator.ts
+++ src/stream/campaign-creator.ts
@@ -1,7 +1,12 @@
 import { FilterNode, Journey } from "../common/journey-types";
 import { uploadJSON } from "../utils/storage";
-import { addContractListener } from "./"
 
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const createCampaign = async (
     name: string,
@@ -14,4 +19,43 @@ export const createCampaign = async (
 
     const streamId = await addContractListener(name, contractAddress, campaignOptionsURI, journey, mainFilter);
     return streamId;
+}
+
+const addContractListener = async (
+    name: string, 
+    contractAddress: string, 
+    campaignOptionsURI: string, 
+    journey: Journey[], 
+    mainFilter?: FilterNode
+) => {
+    try {
+
+        const { event, eventAbi } = journey.find(journeyEntry => journeyEntry.isMain)!;
+
+        const streams = await Moralis.Streams.getAll({ networkType: "evm", limit: 10 });
+        let stream = streams.toJSON().result.find(stream => stream.tag === name);
+
+        if (stream) {
+            return { error: "Contract listener already set." }
+        }
+
+        const streamOptions = {
+            chains: [EvmChain.MUMBAI],
+            description: "monitor smart contract",
+            tag: campaignOptionsURI,
+            webhookUrl: `${process.env.WEBHOOK_URL}/event`,
+            includeContractLogs: true,
+            advancedOptions: mainFilter ? [{ topic0: event!, filter: mainFilter }] : [],
+            abi: eventAbi as any,
+            topic0: [event!]
+        }
+
+        stream = (await Moralis.Streams.add(streamOptions)).toJSON();
+
+        await Moralis.Streams.addAddress({ address: [contractAddress], id: stream.id });
+
+        return { data: { streamId: stream.id } }
+    } catch (err) {
+        console.error("Error: ", err)
+    }
 }
